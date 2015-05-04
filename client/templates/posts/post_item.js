4042ca7e1d42b58b6db8db6365b894eeeba4a6b9
Charles Zhu <charlesmzhu@gmail.com>
2015-05-04 15:42:37 -0700

    With a whole bunch of bugs fixed re: user permissions.

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -1,4 +1,12 @@
 Template.postItem.helpers({
+	//Detemrine author of the current post using this.
+	//Determine if current user is == author of the current post.
+	//return true
+
+	ownPost: function () {
+		return this.userId == Meteor.userId() && this.userId != undefined;
+	},
+
 	domain: function () {
 		var a = document.createElement('a');
 		a.href = this.url;
