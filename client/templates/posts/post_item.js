83d5a32ae851332c8dfe385728a114f3fce4fd3e
Charles Zhu <charlesmzhu@gmail.com>
2015-06-07 13:46:16 -0700

    With post edits

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -1,15 +1,13 @@
 Template.postItem.helpers({
-	//Detemrine author of the current post using this.
-	//Determine if current user is == author of the current post.
-	//return true
-
-	ownPost: function () {
-		return this.userId == Meteor.userId() && this.userId != undefined;
-	},
-
-	domain: function () {
-		var a = document.createElement('a');
-		a.href = this.url;
-		return a.hostname;
-	}
-})
+  ownPost: function() {
+    return this.userId === Meteor.userId();
+  },
+  domain: function() {
+    var a = document.createElement('a');
+    a.href = this.url;
+    return a.hostname;
+  },
+  commentsCount: function() {
+    return Comments.find({postId: this._id}).count();
+  }
+});
